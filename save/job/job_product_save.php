d20073ddd8610c00c0e0a6737b6456e7a0cef0c1
Eranda Palliyaguru <74122774+eranda-palliyaguru@users.noreply.github.com>
2024-11-05 14:08:32 +0530

    update price fm

---
+++ save/job/job_product_save.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+include('../../config.php');
+
+// Get the form data
+$product_id = $_POST['product_id'];
+
+$quantity = $_POST['qty'];
+$job_no = $_POST['job_no']; 
+$date = date('Y-m-d'); 
+$about=$_POST['about'];
+
+
+
+
+
+ 
+$product_name=select_item('products','product_name','id='.$product_id,'../../');
+$price=select_item('products','price','id='.$product_id,'../../');
+
+$result=select('price_fm','*','product_id='.$product_id,'../../');
+while ($row = $result->fetch()) {
+    if($quantity > $row['slb_from'] && $quantity < $row['slb_to']) {
+        $price=$row['price'];
+    }
+}
+
+
+$action = select_item('job','action','id='.$job_no,'../../');
+//echo "$action";
+
+// Prepare the data to be inserted
+$insertData = array(
+    "data" => array(
+        "name" => $product_name,
+        "qty" => $quantity,
+        "job_no" => $job_no,
+        "price" => $price,
+        "amount" => $price*$quantity,
+        "product_id" => $product_id,
+        "date" => $date,
+        "about" => $about,
+        "status" =>'measure',
+    ),
+    "other" => array(
+    ),
+);
+
+// Insert the data into the sales_list table
+insert("sales_list", $insertData, '../../');
+
+// Redirect back to the job details page
+//header("location: ../../job_view.php?id=" . base64_encode($job_no));
+//echo "<script>location.href='../../job_view.php';</script>";
+
+if($action== 2){
+    
+    $result = update('job', ['action'=>3], 'id='.$job_no, '../../');
+    
+   //header("location: ../../job_view.php?id=" . base64_encode($job_no));
+    
+ 
+}
+header("location: ../../job_view.php?id=" . base64_encode($job_no));
+?>

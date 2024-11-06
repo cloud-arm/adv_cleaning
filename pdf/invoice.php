0751fe309f435e4c06b34f786e707d72feb4a0f7
Eranda Palliyaguru <74122774+eranda-palliyaguru@users.noreply.github.com>
2024-11-06 16:43:15 +0530

    up

--- pdf/invoice.php
+++ pdf/invoice.php
@@ -140,7 +140,7 @@ echo $output;
 
 $contact = '0779252594';
 
-    $text = 'Dear [Customers Name],Thank you for choosing Advanced Cleaning Services! Please find your quote attached. We look forward to your feedback and are excited to provide you with our top-quality service.Best regards,The Advanced Cleaning Services Team';
+    $text = 'Dear'. $name.',Thank you for choosing Advanced Cleaning Services! Please find your quote attached. We look forward to your feedback and are excited to provide you with our top-quality service.Best regards,The Advanced Cleaning Services Team';
     $url = get_pdf($output, 'invoice', 'bin/');
     $url = 'https://adcleaning.colorbiz.org/main/pages/forms/pdf/' . $url;
     echo "URL is".$url;
